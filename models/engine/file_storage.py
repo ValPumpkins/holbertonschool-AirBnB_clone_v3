3075208f9da6380e30caeec6c95b2d212d8d0f10
lucieruin <lucie.wattez@hotmail.fr>
2023-12-19 16:34:59 +0100

    add method get and method count for file db_storage & file_storage

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -68,3 +68,14 @@ class FileStorage:
     def close(self):
         """call reload() method for deserializing the JSON file to objects"""
         self.reload()
+
+    def get(self, cls, id):
+        key = cls.__name__ + "." + id
+        if key in self.all(cls):
+            return self.all(cls)[key]
+        return None
+    
+    def count(self, cls=None):
+        if cls is None:
+            return len(self.__objects())
+        return len(self.all(cls.__name__))
